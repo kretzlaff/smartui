d6013ecddfc865d9cea034b4da3b78454106cb45
Kenny Retzlaff <kenny.retzlaff@gmail.com>
2020-12-24 12:14:19 +0100

    add settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    DashboardComponent
+    DashboardComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
